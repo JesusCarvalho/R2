8326ee4d8d471f01511340d4bd47ed2f51435d70
Jesus.Carvalho <Jesus.Carvalho@gmail.com>
2014-06-08 01:43:49 -0400

    DONE:
    1] header.rb, updated nested attributes properties
    
    TODO:
    
    1] Style select2 background to be identical to btn-transparent or input-transparent
    
    2] Add/implement twitter-typeahead to project for FDLE statute lookup and arrest book suggestions
    
    3] add code to create nested/dynamic forms
    
    4] code for file upload (via carrierwave or paperclip)
    
    5] code for comet messaging
    
    6] prawn/pdfwriter code for stamping template
    
    7] Streaming replication to standby servers
    
    8] Clone functionality with offense and everything (datetimes bumped)
    
    9] Search functionality (fuzzy text match)

--- app/models/header.rb
+++ app/models/header.rb
@@ -17,20 +17,20 @@ class Header < ActiveRecord::Base
   has_many :victim_info
 
 
-  accepts_nested_attributes_for :additional_information
-  accepts_nested_attributes_for :narrative
-  accepts_nested_attributes_for :associate
-  accepts_nested_attributes_for :death_investigation
-  accepts_nested_attributes_for :missing_person
-  accepts_nested_attributes_for :multimedias
-  accepts_nested_attributes_for :names_list
-  accepts_nested_attributes_for :offense
-  accepts_nested_attributes_for :officer_assaulted_or_killed
-  accepts_nested_attributes_for :person_description
-  accepts_nested_attributes_for :property_crime
-  accepts_nested_attributes_for :url
-  accepts_nested_attributes_for :vehicle_vessel
-  accepts_nested_attributes_for :victim_info
+  accepts_nested_attributes_for :additional_information,  :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :narrative,               :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :offense,                 :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :associate,                                         :allow_destroy => true
+  accepts_nested_attributes_for :death_investigation,                               :allow_destroy => true
+  accepts_nested_attributes_for :missing_person,                                    :allow_destroy => true
+  accepts_nested_attributes_for :multimedias,                                       :allow_destroy => true
+  accepts_nested_attributes_for :names_list,                                        :allow_destroy => true
+  accepts_nested_attributes_for :officer_assaulted_or_killed,                       :allow_destroy => true
+  accepts_nested_attributes_for :person_description,                                :allow_destroy => true
+  accepts_nested_attributes_for :property_crime,                                    :allow_destroy => true
+  accepts_nested_attributes_for :url,                                               :allow_destroy => true
+  accepts_nested_attributes_for :vehicle_vessel,                                    :allow_destroy => true
+  accepts_nested_attributes_for :victim_info,                                       :allow_destroy => true
 
 
 
