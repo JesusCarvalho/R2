a2ad15d90468f3a561d1b690596be713c4a1aae9
Jesus.Carvalho <Jesus.Carvalho@gmail.com>
2014-06-11 23:38:21 -0400

    DONE:
    1] Emailed phil about select2 background
    
    2] Added columns to associate table for storing datetime and location last seen
    
    3] Updated related fields in missing_person partial
    
    4] Centered checkboxes on AI form partial
    
    TODO:
    
    0] Make Contacts section of MP form partial its own partial (new form section)
    
    1] Style select2 background to be identical to btn-transparent or input-transparent
    
    2] Add/implement twitter-typeahead to project for FDLE statute lookup and arrest book suggestions
    
    3] add code to create nested/dynamic forms
    
    4] code for file upload (via carrierwave or paperclip)
    
    5] code for comet messaging
    
    6] prawn/pdfwriter code for stamping template
    
    7] Streaming replication to standby servers
    
    8] Clone functionality with offense and everything (datetimes bumped)
    
    9] Search functionality (fuzzy text match)

--- app/models/header.rb
+++ app/models/header.rb
@@ -17,20 +17,20 @@ class Header < ActiveRecord::Base
   has_many :victim_info
 
 
-  accepts_nested_attributes_for :additional_information,  :reject_if => :all_blank, :allow_destroy => true
-  accepts_nested_attributes_for :narrative,               :reject_if => :all_blank, :allow_destroy => true
-  accepts_nested_attributes_for :offense,                 :reject_if => :all_blank, :allow_destroy => true
-  accepts_nested_attributes_for :associate,                                         :allow_destroy => true
-  accepts_nested_attributes_for :death_investigation,                               :allow_destroy => true
-  accepts_nested_attributes_for :missing_person,                                    :allow_destroy => true
-  accepts_nested_attributes_for :multimedias,                                       :allow_destroy => true
-  accepts_nested_attributes_for :names_list,                                        :allow_destroy => true
-  accepts_nested_attributes_for :officer_assaulted_or_killed,                       :allow_destroy => true
-  accepts_nested_attributes_for :person_description,                                :allow_destroy => true
-  accepts_nested_attributes_for :property_crime,                                    :allow_destroy => true
-  accepts_nested_attributes_for :url,                                               :allow_destroy => true
-  accepts_nested_attributes_for :vehicle_vessel,                                    :allow_destroy => true
-  accepts_nested_attributes_for :victim_info,                                       :allow_destroy => true
+  accepts_nested_attributes_for :additional_information,      :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :narrative,                   :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :offense,                     :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :associate,                                             :allow_destroy => true
+  accepts_nested_attributes_for :death_investigation,                                   :allow_destroy => true
+  accepts_nested_attributes_for :missing_person,                                        :allow_destroy => true
+  accepts_nested_attributes_for :multimedias,                                           :allow_destroy => true
+  accepts_nested_attributes_for :names_list,                                            :allow_destroy => true
+  accepts_nested_attributes_for :officer_assaulted_or_killed,                           :allow_destroy => true
+  accepts_nested_attributes_for :person_description,                                    :allow_destroy => true
+  accepts_nested_attributes_for :property_crime,                                        :allow_destroy => true
+  accepts_nested_attributes_for :url,                                                   :allow_destroy => true
+  accepts_nested_attributes_for :vehicle_vessel,                                        :allow_destroy => true
+  accepts_nested_attributes_for :victim_info,                                           :allow_destroy => true
 
 
 
